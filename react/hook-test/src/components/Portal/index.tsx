9442f3308a45b78b54b01f9e8a641d66cd0a44e4
shenyuan.will <shenyuan.will@bytedance.com>
2024-04-23 20:33:15 +0800

    feat(react): Portal组件封装

---
+++ react/hook-test/src/components/Portal/index.tsx
@@ -0,0 +1,41 @@
+import { forwardRef, useEffect, useImperativeHandle, useMemo } from "react";
+import { createPortal } from "react-dom";
+
+export interface PortalProps {
+  attach?: HTMLElement | string;
+  children: React.ReactNode;
+}
+
+const getAttach = (attach: PortalProps['attach']) => {
+  if (typeof attach === 'string') {
+    return document.querySelector(attach);
+  };
+  if (typeof attach === 'object' && attach instanceof window.HTMLElement) {
+    return attach;
+  };
+  return document.body;
+}
+
+
+const Portal = forwardRef((props: PortalProps, ref) => {
+  const { attach=document.body, children } = props;
+  const container = useMemo(() => {
+    const el = document.createElement('div');
+    el.className = 'portal-wrapper';
+    return el;
+  }, []);
+
+  useEffect(() => {
+    const parentElement = getAttach(attach);
+    parentElement?.appendChild?.(container);
+    return () => {
+      parentElement?.removeChild?.(container);
+    }
+  }, [container, attach]);
+
+  useImperativeHandle(ref, () => container);
+
+  return createPortal(children, container)
+});
+
+export default Portal;
