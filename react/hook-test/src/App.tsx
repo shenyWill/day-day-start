9be23698ea78812526b45b5d047b37119f309f49
shenyuan.will <shenyuan.will@bytedance.com>
2024-04-23 18:02:46 +0800

    feat(react): 增加Space组件

--- react/hook-test/src/App.tsx
+++ react/hook-test/src/App.tsx
@@ -4,17 +4,24 @@ import './App.css';
 // import Calendar from './Calendar';
 // import dayjs from 'dayjs';
 // import SuspenseTest from './APITest/Suspense';
-import { IconAdd } from './components/Icon/IconAdd';
-import { IconEmail } from './components/Icon/IconEmail';
-import { createFromIconfont } from './components/Icon/createFromtIconfont';
+// import { IconAdd } from './components/Icon/IconAdd';
+// import { IconEmail } from './components/Icon/IconEmail';
+// import { createFromIconfont } from './components/Icon/createFromtIconfont';
+import Space from './components/Space';
+import './app.scss'
 
-const IconFont = createFromIconfont('//at.alicdn.com/t/c/font_4520108_xeag7mtm34a.js');
+// const IconFont = createFromIconfont('//at.alicdn.com/t/c/font_4520108_xeag7mtm34a.js');
 
 function App() {
   return <div data-testid="test">
-    <IconAdd spin></IconAdd>
+    {/* <IconAdd spin></IconAdd>
     <IconEmail></IconEmail>
-    <IconFont type='icon-bingtutubiao' size='40px'></IconFont>
+    <IconFont type='icon-bingtutubiao' size='40px'></IconFont> */}
+    <Space className='container' direction="horizontal" align="end" warp={true} size={['large', 'small']}>
+      <div className='box'>111</div>
+      <div className='box'>222</div>
+      <div className='box'>333</div>
+    </Space>
   </div>
 }
 
